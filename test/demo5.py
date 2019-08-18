c1e7c67537f867b2b736e96a853401497e5e8bd0
nemo <569857936@qq.com>
2019-08-19 00:31:38 +0800

    ddm

--- test/demo5.py
+++ test/demo5.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
 chrome_options.add_argument('--no-sandbox')  # 这个配置很重要
 client = webdriver.Chrome(chrome_options=chrome_options,
-                          executable_path='/root/chromedriver')  # 如果没有把chromedriver加入到PATH中，就需要指明路径
+                          )  # 如果没有把chromedriver加入到PATH中，就需要指明路径
 
 client.get("https://www.baidu.com")
 print(client.page_source.encode('utf-8'))
