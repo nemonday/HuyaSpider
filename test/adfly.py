03a20ea65fee98ddec8d2f6bf74e0910f80a6c3b
nemo <569857936@qq.com>
2019-08-18 23:32:39 +0800

    ddm

--- test/adfly.py
+++ test/adfly.py
@@ -7,22 +7,26 @@ from threading import Thread
 import pymysql
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import selenium.webdriver.support.ui as ui
-from selenium.webdriver.support.ui import WebDriverWait
-from sqlalchemy import create_engine
-from Model import Type, Anchor, Fans
-from sqlalchemy.orm import sessionmaker
-from selenium.webdriver.common.action_chains import ActionChains
 
-from Parameter import PC_USER_ANGENT_LIST
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 
 
 
 def Adfly():
     opt = webdriver.ChromeOptions()
+    PC_USER_ANGENT_LIST = [
+        'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+        # 'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+        # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    ]
     # 随机请求头
     user_angent = choice(PC_USER_ANGENT_LIST)
     # opt.add_argument('user-agent="%s"'% user_angent)
@@ -35,6 +39,8 @@ def Adfly():
 
     try:
         opt.add_argument('--headless')
+        opt.add_argument('--disable-gpu')
+
         proxy_url = 'http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=0&city=0&yys=0&port=2&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
         proxy = requests.get(proxy_url)
         print(proxy.text)
