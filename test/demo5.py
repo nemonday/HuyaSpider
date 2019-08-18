0c9fe4e4d2ece95691b489110c2d749e34896b8f
nemo <569857936@qq.com>
2019-08-19 07:53:27 +0800

    ddm

--- test/demo5.py
+++ test/demo5.py
@@ -26,6 +26,7 @@ def ddm():
         # 'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
         # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
     ]
+
     # 随机请求头
     user_angent = choice(PC_USER_ANGENT_LIST)
     chrome_options.add_argument('user-agent="%s"' % user_angent)
@@ -35,17 +36,22 @@ def ddm():
     chrome_options.add_argument('--no-sandbox')  # 这个配置很重要
     client = webdriver.Chrome(chrome_options=chrome_options,
                               executable_path='/root/ddm/test/chromedriver')  # 如果没有把chromedriver加入到PATH中，就需要指明路径
+    try:
 
-    for i in range(3):
-        urls2 = 'http://dd.ma/GbSfLvyK'
-        client.delete_all_cookies()
-        client.get(urls2)
-        time.sleep(5)
-        client.find_element_by_xpath('//*[@id="btn_open"]/a').click()
-        print('点击')
-        time.sleep(1)
+        for i in range(3):
+            urls2 = 'http://dd.ma/GbSfLvyK'
+            client.delete_all_cookies()
+            client.get(urls2)
+            time.sleep(5)
+            client.find_element_by_xpath('//*[@id="btn_open"]/a').click()
+            print('点击')
+            time.sleep(1)
 
-    client.quit()
+        client.quit()
+
+    except:
+        client.quit()
+
 
 
 while True:
