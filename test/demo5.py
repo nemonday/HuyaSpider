46519a08079ae8d5cc0f001cd54a98ae5fb21545
nemo <569857936@qq.com>
2019-08-19 00:45:58 +0800

    ddm

--- test/demo5.py
+++ test/demo5.py
@@ -1,10 +1,20 @@
+import json
 import time
 
+import requests
 from selenium import webdriver
 
 
 def ddm():
     chrome_options = webdriver.ChromeOptions()
+    proxy_url = 'http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=0&city=0&yys=0&port=2&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
+    proxy = requests.get(proxy_url)
+    print(proxy.text)
+    proxy = json.loads(proxy.text)['data'][0]
+    proxies = {
+        'https': 'http://{0}:{1}'.format(proxy['ip'], proxy['port'])
+    }
+    chrome_options.add_argument("--proxy-server={}".format(proxies['https']))
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')  # 这个配置很重要
