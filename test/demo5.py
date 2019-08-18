7f63312060d472fc17856b76dc90b65203bd6c19
nemo <569857936@qq.com>
2019-08-19 00:02:45 +0800

    ddm

--- test/demo5.py
+++ test/demo5.py
@@ -5,7 +5,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')  # 这个配置很重要
 client = webdriver.Chrome(chrome_options=chrome_options,
-                          executable_path='/home/chromedriver')  # 如果没有把chromedriver加入到PATH中，就需要指明路径
+                          executable_path='/root/chromedriver')  # 如果没有把chromedriver加入到PATH中，就需要指明路径
 
 client.get("https://www.baidu.com")
 print(client.page_source.encode('utf-8'))
