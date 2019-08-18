d071c1d83d4d31ead19b137bbb86d98a678515c9
nemo <569857936@qq.com>
2019-08-19 06:49:14 +0800

    ddm

--- test/adfly.py
+++ test/adfly.py
@@ -29,7 +29,7 @@ def Adfly():
     ]
     # 随机请求头
     user_angent = choice(PC_USER_ANGENT_LIST)
-    # opt.add_argument('user-agent="%s"'% user_angent)
+    opt.add_argument('user-agent="%s"'% user_angent)
     # 无界面模式，需要看界操作，注释此行
     # opt.add_argument('--headless')
     # 部署服务器上 需要使用此两行代码
@@ -55,7 +55,7 @@ def Adfly():
         broser.maximize_window()
 
         for i in range(3):
-            urls2 = 'http://dd.ma/4XmpxwTg'
+            urls2 = 'http://dd.ma/GbSfLvyK'
             broser.delete_all_cookies()
             broser.get(urls2)
             time.sleep(5)
--- test/demo5.py
+++ test/demo5.py
@@ -1,5 +1,6 @@
 import json
 import time
+from random import choice
 
 import requests
 from selenium import webdriver
@@ -14,6 +15,20 @@ def ddm():
     proxies = {
         'https': 'http://{0}:{1}'.format(proxy['ip'], proxy['port'])
     }
+    PC_USER_ANGENT_LIST = [
+        'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+        # 'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+        # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    ]
+    # 随机请求头
+    user_angent = choice(PC_USER_ANGENT_LIST)
+    chrome_options.add_argument('user-agent="%s"' % user_angent)
     chrome_options.add_argument("--proxy-server={}".format(proxies['https']))
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
@@ -22,7 +37,7 @@ def ddm():
                               executable_path='/root/ddm/test/chromedriver')  # 如果没有把chromedriver加入到PATH中，就需要指明路径
 
     for i in range(3):
-        urls2 = 'http://dd.ma/4XmpxwTg'
+        urls2 = 'http://dd.ma/GbSfLvyK'
         client.delete_all_cookies()
         client.get(urls2)
         time.sleep(5)
