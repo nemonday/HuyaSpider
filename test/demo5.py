4b106509d6a60756c0eedf4cd6b170170fadcc9d
nemo <569857936@qq.com>
2019-08-20 15:15:20 +0800

    ddm

--- test/demo5.py
+++ test/demo5.py
@@ -30,6 +30,7 @@ def ddm():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')  # 这个配置很重要
     client = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path='/root/ddm/test/chromedriver'
                               )  # 如果没有把chromedriver加入到PATH中，就需要指明路径
     try:
 
